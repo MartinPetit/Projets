8b0da1bf5765d07e8091438f022c71bee7b0b7f6
MartinPetit <martinpetit1998@gmail.com>
2019-09-26 18:57:06 +0200

    modif final supp

--- supp_donnees.php
+++ supp_donnees.php
@@ -15,4 +15,54 @@ $sql ="DELETE FROM user WHERE id = '".$id."'";
                 $sth = $link->prepare($sql);
                 $sth->execute();
 
+
+$sqli = "SELECT * FROM user";
+$stmm = $link->prepare($sqli);
+$stmm->execute();
+
 ?>
+
+<html>
+<head>
+	<link rel="stylesheet" href="Liste.css" />
+</head>
+<body>
+<h1>liste des user</h1>
+
+<?php 
+
+
+
+foreach ($stmm as $user) { ?>
+
+	<table>
+
+	<tr>
+
+<td> <?php echo $user['nom']; ?> </td>
+<td> <?php echo $user['prenom']; ?> </td>
+<td> <?php echo $user['email']; ?> </td>
+
+<td> <?php echo $user['id']; ?> </td>
+
+
+
+<td> <?php echo " <a href=\"supp_donnees.php?id=".$user['id']."\">Supprimmer</a>\n";?>
+
+
+</tr>
+
+<?php }?>
+
+</table>
+
+
+<br/>
+
+<a href="insert_donnees.php"> Ajouter un utilisateur</a><br/>
+
+
+</body>
+</html>
+
+
